f8f7e752c1328af15a965328e1b50fd963f12a04
miillopp <vsevolod.milop@gmail.com>
2025-09-30 09:32:11 +0300

    Update zadanie1.py

---
+++ zadanie3.py
@@ -0,0 +1,7 @@
+from math import *
+a = float(input("введите число a: "))
+b = float(input("введите число b: "))
+c = float(input("введите число c: "))
+d = float(input("введите число d: "))
+result = (a / c) * (b / d) - ((a * b) - c) / (c * d)
+print(result)
