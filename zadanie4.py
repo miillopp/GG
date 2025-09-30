929ed3102ccab065024442c3e99a7de1821b10da
miillopp <vsevolod.milop@gmail.com>
2025-09-30 18:31:28 +0300

    Update zadanie4.py

--- zadanie4.py
+++ zadanie4.py
@@ -2,7 +2,4 @@ x, y = map(float, input().split())
 r1 = 3
 r2 = 5
 t = x + y 
-if (t >= r1) and (t <= r2) and (x >= 0):
-    print(True)
-else:
-    print(False)
+print ((t >= r1) and (t <= r2) and (x >= 0))
